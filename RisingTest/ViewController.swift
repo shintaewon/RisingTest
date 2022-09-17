493168717fb82c1eeb8326e6e4b5ceecd252f4e2
Taewon Shin <tlsxodnjs147@gmail.com>
2022-09-17 17:12:15 +0900

    Initial Commit

---
+++ RisingTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RisingTest
+//
+//  Created by 신태원 on 2022/09/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
